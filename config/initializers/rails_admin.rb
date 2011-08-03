f625e3f43aa639f56d07f7c854f150577e061772
Ricardo Duarte <ricardo@ricardoduarte.com>
2011-08-03 00:46:11 -0300

    Adicionado campo cached_slug no ignore do RailsAdmin para os modelos de Produtos e Categorias.

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,5 +1,8 @@
 RailsAdmin.config do |config|
   config.model Category do
-    exclude_fields :lft, :rgt
+    exclude_fields :lft, :rgt, :cached_slug
+  end
+  config.model Product do
+    exclude_fields :cached_slug
   end
 end
