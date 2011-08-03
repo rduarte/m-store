bfac64a27304b3199d93bb257be6db85e977cee9
Ricardo Duarte <ricardo@ricardoduarte.com>
2011-08-03 00:29:09 -0300

    Adicionando consulta de categorias no layout. O estilo ainda é provisório.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :load_categories  
+
+  def load_categories
+    @categories = Category.root.self_and_siblings
+  end
+
 end
