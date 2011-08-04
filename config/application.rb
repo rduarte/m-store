a5704291ad23840295762b07c8a3094ef3aea8ae
Ricardo Duarte <ricardo@ricardoduarte.com>
2011-08-04 00:37:18 -0300

    Bugfix: Adicionando configuração para locale funcionar no Heroku.

--- config/application.rb
+++ config/application.rb
@@ -27,8 +27,9 @@ module MStore
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    #config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :"pt-BR"
+    config.i18n.locale = :"pt-BR"
 
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
