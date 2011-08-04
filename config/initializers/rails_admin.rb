2b50b311f6c5ec56faf452a2a712eda978db4fc4
Ricardo Duarte <ricardo@ricardoduarte.com>
2011-08-03 23:58:18 -0300

    MonkeyPatch: Correção para compatibilidade entre RaisAdmin e friendly_id

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,3 +1,13 @@
+require "rails_admin/adapters/active_record"
+
+module RailsAdmin::Adapters::ActiveRecord
+  def get(id)
+    if object = model.find(id)
+      RailsAdmin::AbstractObject.new object
+    end
+  end
+end
+
 RailsAdmin.config do |config|
   config.model Category do
     exclude_fields :lft, :rgt, :cached_slug
